13ab17368953688af2c7b8e78f9f1454d288a013
Hitesh Modi <87275343+hiteshmodi0624@users.noreply.github.com>
2023-03-26 03:17:49 +0530

    Update post-header.tsx

--- components/posts/post-details/post-header.tsx
+++ components/posts/post-details/post-header.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { FC } from "react";
 
 const PostHeader:FC<{title:string,image:string}>=(props)=>{
-    return <header className="flex">
+    return <header className="flex flex-wrap">
         <h1 className="text-purple-900 font-extrabold text-6xl">{props.title}</h1>
         <Image src={props.image} height={150} width={200} alt={props.image}/>
     </header>
