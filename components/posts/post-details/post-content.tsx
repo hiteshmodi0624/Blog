30865d383cd3af5513434c9e6bdaaf504183f206
Hitesh <hitesh_12014132@nitkkr.ac.in>
2023-03-26 02:14:42 +0530

    optimising

--- components/posts/post-details/post-content.tsx
+++ components/posts/post-details/post-content.tsx
@@ -4,8 +4,13 @@ import { FC } from "react"
 import post from "@/modals/post"
 import styles from "./post-content.module.css"
 import Image from "next/image"
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
-import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism"
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter"
+import atomDark from "react-syntax-highlighter/dist/cjs/styles/prism/atom-dark"
+import js from "react-syntax-highlighter/dist/cjs/languages/prism/javascript"
+import css from "react-syntax-highlighter/dist/cjs/languages/prism/css"
+
+SyntaxHighlighter.registerLanguage('js',js);
+SyntaxHighlighter.registerLanguage('css',css);
 const PostContent:FC<{post:post}>=({post})=>{
     const imageUrl=`/images/posts/${post.slug}/${post.image}`
 
